5172a7a31317b802cd06caf1dc52e2ad154f41d3
MargaritaWendt <margaritaortizreategui@gmail.com>
2026-02-14 21:31:50 +0100

    style footer and header

---
+++ final/scripts/date.js
@@ -0,0 +1,5 @@
+const currentYear = document.querySelector("#currentyear");
+currentYear.textContent = new Date().getFullYear();
+
+const lastModifiedP = document.querySelector("#last-modified");
+lastModifiedP.textContent = document.lastModified;
